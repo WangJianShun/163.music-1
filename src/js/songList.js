716723a0a69db83967464ad855dc2939b3475165
Wang <1779011185@qq.com>
2018-07-31 20:13:17 +0800

    刷新页面依然显示歌曲列表

--- src/js/songList.js
+++ src/js/songList.js
@@ -28,6 +28,15 @@
   let model = {
     data: {
       songs: []
+    },
+    find(){
+      var query=new AV.Query('Song')
+      return query.find().then((songs)=>{
+          this.data.songs=songs.map((song)=>{
+            return {id:song.id,...song.attributes}
+          })
+          return songs
+      })
     }
   }
   let controller = {
@@ -41,6 +50,9 @@
       window.eventHub.on('creat', (songData) => {
         console.log(songData)
         this.model.data.songs.push(songData)
+        this.view.render(this.model.data)
+      })
+      this.model.find().then(()=>{
         this.view.render(this.model.data)
       })
     }
