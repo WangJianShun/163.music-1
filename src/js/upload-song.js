cb71e0283d05b110a96bfcbebe09bbfc04a36cb2
Wang <1779011185@qq.com>
2018-08-03 21:35:29 +0800

    上传歌曲添加loading动画

--- src/js/upload-song.js
+++ src/js/upload-song.js
@@ -37,12 +37,14 @@
           },
           'BeforeUpload': function (up, file) {
             // 每个文件上传前,处理相关的事情
+            window.eventHub.emit('beforeUpload')
           },
           'UploadProgress': function (up, file) {
             //uploadStatus.textContent = "文件上传中"
             // 每个文件上传时,处理相关的事情
           },
           'FileUploaded': function (up, file, info) {
+            window.eventHub.emit('afterUpload')
 
             // uploadStatus.textContent = "文件上传成功"
 
