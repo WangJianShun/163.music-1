1186a18508627a0554e2b1baaf74d8d26a3691a8
Wang <1779011185@qq.com>
2018-08-16 21:50:01 +0800

    做播放界面背景图模糊效果

--- src/js/song/app.js
+++ src/js/song/app.js
@@ -5,7 +5,7 @@ let view = {
   },
   render(data) {
     let { song, status } = data
-    $(this.el).css('background-image', `url('${song.cover}')`)
+    $('.bgimg').css('background-image', `url('${song.cover}')`)
     $(this.el).find('img.cover').attr('src', song.cover)
 
     if ($(this.el).find('audio').attr('src') !== song.url) {
